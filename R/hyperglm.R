b4cb00b7b7584391d23fc83efe287e294835ee3b
alepigna12 <apignatelli@college.harvard.edu>
2024-02-05 23:24:08 -0500

    Adding R scripts

---
+++ R/hyperglm.R
@@ -0,0 +1,6 @@
+hiper_glm <- function (outcome, design) {
+  #TODO: Maximize likelihood
+  hglm <- list()
+  class(hglm) <- "hglm"
+  return(hglm)
+}
---
+++ R/model_output.R
@@ -0,0 +1,12 @@
+predict.hglm <- function(hglm) {
+  #Implement prediction
+}
+coef.hglm <- function (hglm) {
+  #Return coefficients
+}
+vcov.hglm <- function (hglm) {
+  #Return var-covar matrix
+}
+logistic_output.hglm <- function(hglm) {
+  print("Output of hiper_glm()")
+}
