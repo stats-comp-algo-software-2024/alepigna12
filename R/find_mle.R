8d6f37b4a5cd8eadefe4b0d60ebbf01669a0ba5b
alepigna12 <apignatelli@college.harvard.edu>
2024-02-22 14:30:12 -0500

    Conversion

--- R/find_mle.R
+++ R/find_mle.R
@@ -1,9 +1,21 @@
 find_mle_pseudo_inverse <- function(design, outcome) {
-  as.vector(solve(t(design)%*%design, t(design)%*%outcome))
+  beta = as.vector(solve(t(design)%*%design, t(design)%*%outcome))
+  noise_variance = mean((outcome - design%*%beta)^2)
+  n = nrow(design)
+  p = ncol(design)
+  noise_variance = noise_variance/(1-p/n)
+  info = t(design)%*%design/noise_variance
+  return(list(beta_mle=beta, information=info))
 }
 
-find_mle_BFGS <- function(beta_start, design, outcome, noise_var=1) {
-  optim(beta_start, lm_likelihood, grad_likelihood, design=design, outcome=outcome, method="BFGS", control=list(fnscale=-1, abstol=1e-9))
+find_mle_BFGS <- function(beta_start, design, outcome, noise_variance=1, model="linear") {
+  if(model == "linear"){
+    model_res = optim(beta_start, lm_likelihood, grad_likelihood, design=design, outcome=outcome, method="BFGS", control=list(fnscale=-1, abstol=1e-9), hessian=TRUE)
+  }
+  else if(model == "logit"){
+    model_res = optim(beta_start, logistic_loglikelihood, logistic_grad_loglikelihood, design=design, outcome=outcome, method="BFGS", control=list(fnscale=-1, abstol=1e-9), hessian=TRUE)
+  }
+  return(list(beta_mle=model_res$par, information=-model_res$hessian))
 }
 
 lm_likelihood <- function(beta, design, outcome, noise_variance=1) {
@@ -12,6 +24,25 @@ lm_likelihood <- function(beta, design, outcome, noise_variance=1) {
 
 grad_likelihood <- function(beta, design, outcome, noise_variance=1) {
   1/noise_variance*(t(design)%*%outcome - t(design)%*%design%*%beta)
+}
+
+find_mle_newton <- function(beta_start, design, outcome){
+  beta = beta_start
+  n_iter = 0
+  change = 5
+  abs_tol = 0.05*(qchisq(0.975,1) - qchisq(0.025,1))
+  while(!are_all_close(change,0,abs_tol)){
+    hessian = logistic_hessian(beta,design,outcome)
+    gradient = logistic_grad_loglikelihood(beta,design,outcome)
+    curr_lik = logistic_loglikelihood(beta,design,outcome)
+    beta = beta + solve(hessian,gradient)
+    change = abs(logistic_loglikelihood(beta,design,outcome) - curr_lik)
+    n_iter = n_iter + 1
+    if(n_iter == 100000){
+      stop("Algorithm did not converge")
+    }
+  }
+  return(list(beta_MLE=beta, information=-logistic_hessian(beta,design,outcome)))
 }
 
 logistic_loglikelihood <- function(beta, design, outcome) {
--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -4,21 +4,21 @@ test_that("MLE via BFGS matches result via pseudo-inverse", {
   design = data$design
   outcome = data$outcome
   beta = rep(0.1, ncol(design))
-  mle_pseudo = find_mle_pseudo_inverse(design, outcome)
-  mle_optim = find_mle_BFGS(beta, design, outcome)$par
+  mle_pseudo = find_mle_pseudo_inverse(design, outcome)$beta_mle
+  mle_optim = find_mle_BFGS(beta, design, outcome)$beta_mle
   result = are_all_close(mle_pseudo, mle_optim, rel_tol = 1e-4)
   expect_true(result)
 })
 
 test_that("newton and bfgs outputs coincide on logit model", {
-  n_obs <- 32; n_pred <- 4
-  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  n_obs <- 32; n_pred <- 2
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 111)
   design <- data$design; outcome <- data$outcome
-  via_newton_out <- hiper_glm(design, outcome, model = 'logit')
+  via_newton_out <- hiper_glm(outcome, design, model = 'logit', method = 'newton')
   via_bfgs_out <- hiper_glm(
-    design, outcome, model = 'logit', method = 'BFGS'
+    outcome, design, model = 'logit', method = 'BFGS'
   )
   expect_true(are_all_close(
-    coef(via_newton_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
+    via_newton_out, via_bfgs_out, abs_tol = 1e-2, rel_tol = 1e-2
   ))
 })
